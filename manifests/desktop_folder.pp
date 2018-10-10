0bdcb9395a9b2e683dc895e1f33a90cab10caf90
Dan Iverson <dan.iverson@minneapolismn.gov>
2018-10-10 09:02:18 -0500

    Create Desktop folder in 32-bit directory

--- manifests/desktop_folder.pp
+++ manifests/desktop_folder.pp
@@ -2,8 +2,13 @@ class io_appserver::desktop_folder (
   $appserver_domain_list  = $io_appserver::appserver_domain_list,
 ){
 
-  file { 'C:/Windows/System32/config/systemprofile/desktop':
+  file { 'desktop-folder-64':
+    path => 'C:/Windows/System32/config/systemprofile/Desktop':
     ensure    => directory,
   }
 
+  file {'desktop-folder-32':
+    path => 'C:/Windows/SysWOW64/config/systemprofile/Desktop',
+    ensure => folder,
+  }
 }
